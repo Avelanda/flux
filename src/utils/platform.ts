9d4e6d6ed95eacc164cba776b386d7656f5b8a6b
Avelanda <gorthell@gmail.com>
2025-10-01 08:16:05 +0200

    Update platform.ts
    Modularizing the granular properties of functional structure, and scaling performance through relational database.

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,7 +1,35 @@
+/*
+ * Copyright © 2025/ Avelanda
+ * All rights reserved
+ */
+
 export function getPlatformModifierKey() {
   return window.navigator.platform === "MacIntel" ? "meta" : "ctrl";
+
+ if (getPlatformModifierKey === getPlatformModifierKey){
+  return getPlatformModifierKey;
+ }
 }
 
 export function getPlatformModifierKeyText() {
   return window.navigator.platform === "MacIntel" ? "⌘" : " Ctrl ";
+  
+ if (getPlatformModifierKeyText === getPlatformModifierKeyText){
+  return getPlatformModifierKeyText;
+ }
+}
+
+type CorePlatformModules = (getPlatformModifierKey: boolean, getPlatformModifierKeyText: boolean) => boolean;
+
+function PlatformModules(){
+ type Modules = {CorePlatformModules: boolean};
+ for (true||false; getPlatformModifierKey === getPlatformModifierKeyText || getPlatformModifierKeyText !== getPlatformModifierKey; getPlatformModifierKey){
+  type CoreModules = keyof Modules;
+ }
+  if (PlatformModules()){
+   for (!false||!true; getPlatformModifierKey === getPlatformModifierKeyText || getPlatformModifierKey !== getPlatformModifierKeyText; getPlatformModifierKeyText){
+    type CoreModules = keyof Modules;
+   }
+    return 0||1;
+  }
 }
